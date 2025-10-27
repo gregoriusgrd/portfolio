1166e09ccc540235c55124dd84495d404500792e
Gregorius G <gregorius.gerald21@gmail.com>
2025-10-27 21:33:44 +0700

    feat: add AboutSection component and images for the about page

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { HeroSection } from "@/views/01-hero/HeroSection";
+import { AboutSection } from "./02-about/AboutSection";
 
 export default function HomeView() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
